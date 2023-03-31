4c5d3155b7d1e82e01a9ea3849a5bacda3c73515
jpra2 <jprandrade2@gmail.com>
2023-03-30 22:17:14 -0300

    lista2 finish

--- lista2/ex_6.m
+++ lista2/ex_6.m
@@ -8,12 +8,34 @@ intervalo = [1, 20];
 % a)
 delta = intervalo(2) - intervalo(1);
 test = rand(1, m);
-areas = intervalo(1) + delta*test;
+areas = intervalo(1) + sort(delta*test);
 
 % b)
 tensoes = N./areas;
 
 % c)
+% plot(areas, tensoes);
+
+% d) e)
+n = 5;
+randomic = sort(randperm(m, n));
+areas_rand = areas(randomic);
+tensoes2 = N./areas_rand;
+% plot(areas_rand, tensoes2);
+
+% f)
+X = areas;
+Y = tensoes;
+
+lenx = length(X)
+newl = 3*lenx
+min_X = min(X);
+max_X = max(X);
+delta_X = max_X - min_X;
+test = rand(1, newl);
+areas_interp = min_X + sort(delta_X*test);
+tensoes_interp = interp1(areas,tensoes,areas_interp);
+plot(areas_interp, tensoes_interp);
+hold on
 plot(areas, tensoes)
-
-
+legend({'grafico1', 'grafico interpolado'});
