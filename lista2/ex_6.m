59a59ebcdeefd53186646ba7e9df0d60fc2fc9ac
jpra2 <jprandrade2@gmail.com>
2023-04-08 21:36:58 -0300

    update ex_6 lista2

--- lista2/ex_6.m
+++ lista2/ex_6.m
@@ -14,21 +14,21 @@ areas = intervalo(1) + sort(delta*test);
 tensoes = N./areas;
 
 % c)
-% plot(areas, tensoes);
+plot(areas, tensoes);
 
 % d) e)
 n = 5;
 randomic = sort(randperm(m, n));
 areas_rand = areas(randomic);
 tensoes2 = N./areas_rand;
-% plot(areas_rand, tensoes2);
+plot(areas_rand, tensoes2);
 
 % f)
 X = areas;
 Y = tensoes;
 
-lenx = length(X)
-newl = 3*lenx
+lenx = length(X);
+newl = 3*lenx;
 min_X = min(X);
 max_X = max(X);
 delta_X = max_X - min_X;
@@ -37,5 +37,6 @@ areas_interp = min_X + sort(delta_X*test);
 tensoes_interp = interp1(areas,tensoes,areas_interp);
 plot(areas_interp, tensoes_interp);
 hold on
-plot(areas, tensoes)
-legend({'grafico1', 'grafico interpolado'});
+tensoes_real = N./areas_interp;
+plot(areas_interp, tensoes_real)
+legend({'interpolado', 'real'});
