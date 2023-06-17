d8493544b9c992f520f2b368bea0bb8abd93c081
jpra2 <jprandrade2@gmail.com>
2023-06-17 18:04:09 -0300

    update

--- simulacao_reserv/teste1/generate_random_perm.py
+++ simulacao_reserv/teste1/generate_random_perm.py
@@ -1,24 +1,36 @@
 import scipy.stats as stats
 import numpy as np
 
-a, b = 0.001, 100
-mu, sigma = (a+b)/2, 100
+# a, b = 0.001, 100
+# mu, sigma = (a+b)/2, 100
+# dist = stats.truncnorm((a - mu) / sigma, (b - mu) / sigma, loc=mu, scale=sigma)
 
+# values = dist.rvs(17*17).reshape((17, 17))
+# ldist = np.log10(values)
+# vtest = ldist.min()
+
+# while vtest > -1:
+#     values = dist.rvs(17*17).reshape((17, 17))
+#     ldist = np.log10(values)
+#     vtest = ldist.min()
+
+# values = dist.rvs(17*17).reshape((17, 17))
+# values = np.round(values, decimals=2)
+
+# all_lines = []
+# for line in values:
+#     line_str = [str(val) for val in line]
+#     linew = ' '.join(line_str)
+#     linew += '\n'
+#     all_lines.append(linew)
+
+a, b = -3, 3
+mu, sigma = 0, 1.5
 dist = stats.truncnorm((a - mu) / sigma, (b - mu) / sigma, loc=mu, scale=sigma)
 
-values = dist.rvs(17*17).reshape((17, 17))
-ldist = np.log10(values)
-vtest = ldist.min()
-
-while vtest > -1:
-    values = dist.rvs(17*17).reshape((17, 17))
-    ldist = np.log10(values)
-    vtest = ldist.min()
-
-values = np.round(values, decimals=2)
-print(values.min())
-print(values.max())
-
+values = dist.rvs(17*17)
+values = np.array([10**i for i in values]).reshape((17, 17))
+values = np.round(values, decimals=3)
 
 all_lines = []
 for line in values:
@@ -27,7 +39,7 @@ for line in values:
     linew += '\n'
     all_lines.append(linew)
 
-with open('perms.txt', 'w') as f:
+with open('perms2.txt', 'w') as f:
     for line in all_lines:
         f.write(line)
 
