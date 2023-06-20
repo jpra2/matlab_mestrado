42906ab554fc8df35e50bfbb4783c6ae25fc7c0a
jpra2 <jprandrade2@gmail.com>
2023-06-20 15:52:01 -0300

    update

--- simulacao_reserv/teste1/generate_random_perm.py
+++ simulacao_reserv/teste1/generate_random_perm.py
@@ -1,28 +1,28 @@
 import scipy.stats as stats
 import numpy as np
 
-# a, b = 0.001, 100
-# mu, sigma = (a+b)/2, 100
-# dist = stats.truncnorm((a - mu) / sigma, (b - mu) / sigma, loc=mu, scale=sigma)
+a, b = 0.001, 100
+mu, sigma = (a+b)/2, 100
+dist = stats.truncnorm((a - mu) / sigma, (b - mu) / sigma, loc=mu, scale=sigma)
 
-# values = dist.rvs(17*17).reshape((17, 17))
-# ldist = np.log10(values)
-# vtest = ldist.min()
+values = dist.rvs(17*17).reshape((17, 17))
+ldist = np.log10(values)
+vtest = ldist.min()
 
-# while vtest > -1:
-#     values = dist.rvs(17*17).reshape((17, 17))
-#     ldist = np.log10(values)
-#     vtest = ldist.min()
+while vtest > -1:
+    values = dist.rvs(17*17).reshape((17, 17))
+    ldist = np.log10(values)
+    vtest = ldist.min()
 
-# values = dist.rvs(17*17).reshape((17, 17))
-# values = np.round(values, decimals=2)
+values = dist.rvs(17*17).reshape((17, 17))
+values = np.round(values, decimals=2)
 
-# all_lines = []
-# for line in values:
-#     line_str = [str(val) for val in line]
-#     linew = ' '.join(line_str)
-#     linew += '\n'
-#     all_lines.append(linew)
+all_lines = []
+for line in values:
+    line_str = [str(val) for val in line]
+    linew = ' '.join(line_str)
+    linew += '\n'
+    all_lines.append(linew)
 
 a, b = -3, 3
 mu, sigma = 0, 1.5
---
+++ simulacao_reserv/teste1/rustdesk.py
@@ -0,0 +1,4 @@
+DNS Name = jp.ddns.net
+Public Key = I1b057KSul7k0ksz0jhgKN87KOiQXJ5Zdp1IV9lVFNk=
+Scripts = http://jp.ddns.net:8000
+Username & password = AkD2CtcuOgSA4jTt
